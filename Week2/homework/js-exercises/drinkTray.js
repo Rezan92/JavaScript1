18c69b88649cf464235771b377dbc700cbfbee4b
Rezan92 <rezanibrahim8@gmail.com>
2020-07-21 03:29:30 +0200

    Improved the logic for adding the array's elements two times to another array

--- Week2/homework/js-exercises/drinkTray.js
+++ Week2/homework/js-exercises/drinkTray.js
@@ -2,30 +2,36 @@
 
 /* The code in this file will add the elements from the array "drinkTypes" to the array "drinkTray",
 each element will be repeated twice.
-when the "drinkTray.length = 6" the loop will stop "break" */
+when the "drinkTray.length = 6" the loop will stop "break" but in this caseit won't break because
+the loop will only run five times*/
 
 const drinkTray = [];
 const drinkTypes = ["cola", "lemonade", "water"];
 
 for (let i = 0; i < 5; i++) {
   let index;
-  if (drinkTray.length <= 1) {
-    index = 0;
+  if (i % 2 === 0) {
+    index = i / 2;
     drinkTray.push(drinkTypes[index]);
-  } else if (drinkTray.length >= 2 && drinkTray.length <= 3) {
-    index = 1;
+  } else {
+    index = (i - 1) / 2;
     drinkTray.push(drinkTypes[index]);
-  } else if (drinkTray.length >= 4 && drinkTray.length <= 5) {
-    index = 2;
-    drinkTray.push(drinkTypes[index]);
-  }else {
+  }
+  if (drinkTray.length === 6) {
     break;
-  };
-};
-console.log(drinkTray);
-
-let drinks= "";
-for(const drink of drinkTray){
-  (drink === "water")? drinks += `${drink}`: drinks +=`${drink}, `;
+  }
 }
+
+/*
+The following code will add a comma and space after each element from the "drinkTray" array except the last element.
+and asign all the elements to the variable "drinks"
+*/
+let drinks = "";
+for (let i = 0; i < drinkTray.length; i++) {
+  let drink = drinkTray[i]
+  if (i === drinkTray.length - 1) {
+    drinks += `${drink}`;
+  } else { drinks += `${drink}, `; }
+}
+console.log(drinkTray);
 console.log(`Hey guys, I brought a ${drinks}!`)
